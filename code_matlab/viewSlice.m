414edc3259beecbdd0ec4c7bcccb81767449ecbc
sakinkirti <sakin.arya.kirti@gmail.com>
2022-05-30 15:47:42 -0700

    reorganized matlab code and began standardization

--- code_matlab/viewSlice.m
+++
@@ -1,2 +0,0 @@
-lesion = niftiread('/Users/sakinkirti/Programming/Python/CCIPD/racial-disparity-pca/dataset/to_nifti_cleaned/prostate-000011/ADC_reg.nii.gz');
-slice = sliceViewer(lesion);
---
+++ code_matlab/view_slice.m
@@ -0,0 +1,2 @@
+lesion = niftiread('/Users/sakinkirti/Programming/Python/CCIPD/racial-disparity-pca/dataset/to_nifti_cleaned/prostate-000011/ADC_reg.nii.gz');
+slice = sliceViewer(lesion);
