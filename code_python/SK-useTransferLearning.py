2001d15644f611e5bb10ef491957471a57bbedbe
sakinkirti <sakin.arya.kirti@gmail.com>
2022-06-03 20:44:11 -0700

    beginning transfer learning approach

---
+++ code_python/SK-useTransferLearning.py
@@ -0,0 +1,20 @@
+import torch
+
+'''
+author Sakin Kirti
+date 6/3/2022
+
+script to use transfer learning from Ansh's SqueezeNet model
+'''
+
+def main():
+    straight_through()
+
+'''
+method to directly use one of the pretrained models
+'''
+def straight_through():
+    
+
+if __name__ == '__main__':
+    main()
