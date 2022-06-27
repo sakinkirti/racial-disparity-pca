7c0582a569ac69ccfcbbaa1db5140e687a862d97
sakinkirti <sakin.arya.kirti@gmail.com>
2022-06-27 09:08:31 -0700

    removing unnecessary files

--- code_python/SK-testing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-path = '/Users/sakinkirti/Programming/Python/CCIPD/racial-disparity-pca/dataset/racial_disparity_FINAL_batch1_anonymized.csv'
-save = '/Users/sakinkirti/Programming/Python/CCIPD/racial-disparity-pca/dataset/racial_disparity_filtered.csv'
-
-df = pd.read_csv(path)
-df = df[df['GGG (1-5)'].notnull()]
-
-df.to_csv(save)
